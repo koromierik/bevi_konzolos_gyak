8c40458e605e845cfda3fe97852a8ce220e4c069
MatiasX <waldorfosmatyi@gmail.com>
2024-04-17 10:22:53 +0200

    up

--- database/create.sql
+++ database/create.sql
@@ -9,6 +9,16 @@ use bevi;
 
 
 create table customers (
-    
+    id int not null primary key auto_increment,
+    name varchar(50),
+    email varchar(50)
+);
 
-)
+
+create table incomings (
+    id int not null primary key auto_increment,
+    total double,
+    date date,
+    source varchar(50),
+    customerId int
+);
