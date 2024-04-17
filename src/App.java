365e0ca31d4468a538cfa083e66960e57559cee2
MatiasX <waldorfosmatyi@gmail.com>
2024-04-17 10:07:57 +0200

    up

--- src/App.java
+++ src/App.java
@@ -1,5 +1,17 @@
+import java.util.ArrayList;
+
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println(args[0]);
+        Filehandler filehandler = new Filehandler();
+        
+        filehandler.readCostumers(args[0]);
+       
+        ArrayList<Customer> customerList = filehandler.readCostumers(args[0]);
+        ArrayList<Incoming> incomingList = filehandler.readIncomings(args[1]);
+
+        for (Incoming incoming : incomingList) {
+            System.out.println(incoming.date);
+
     }
 }
