caafa50bd7a178fe32eee615b5dccadc4bbf24b1
MatiasX <waldorfosmatyi@gmail.com>
2024-04-17 09:10:33 +0200

    up

--- src/Filehandler.java
+++ src/Filehandler.java
@@ -1,8 +1,27 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class Filehandler {
-    public void readCostumers() {
+    public void readCostumers(String filename) throws FileNotFoundException {
+        File file = new File(filename);
+        Scanner scanner = new Scanner(file);
+        scanner.nextLine();
+        while (scanner.hasNext()) {
+            String line = scanner.nextLine();
+            String[] lineArray = line.split(":");
+        }
+
     }
 
-    public void readIncomings() {
+    public void readIncomings(String filename) throws FileNotFoundException {
+        File file = new File(filename);
+        Scanner scanner = new Scanner(file);
+        scanner.nextLine();
+        while (scanner.hasNext()) {
+            String line = scanner.nextLine();
+            String[] lineArray = line.split(":");
+        }
     }
 
 }
