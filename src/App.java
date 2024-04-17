11243cb562c11a40bf73c834feb0733b3e9b1cde
MatiasX <waldorfosmatyi@gmail.com>
2024-04-17 10:08:08 +0200

    up

--- src/App.java
+++ src/App.java
@@ -4,14 +4,15 @@ public class App {
     public static void main(String[] args) throws Exception {
         System.out.println(args[0]);
         Filehandler filehandler = new Filehandler();
-        
+
         filehandler.readCostumers(args[0]);
-       
+
         ArrayList<Customer> customerList = filehandler.readCostumers(args[0]);
         ArrayList<Incoming> incomingList = filehandler.readIncomings(args[1]);
 
         for (Incoming incoming : incomingList) {
             System.out.println(incoming.date);
 
+        }
     }
 }
