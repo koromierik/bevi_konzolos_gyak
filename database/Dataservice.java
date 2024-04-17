497d30c01e42e8577c204ea4cfbeb3d2ccea78a5
MatiasX <waldorfosmatyi@gmail.com>
2024-04-17 10:25:13 +0200

    up

---
+++ database/Dataservice.java
@@ -0,0 +1,10 @@
+public class Dataservice {
+    public void insertCustomers(ArrayList<Customer> customerList) {
+
+    }
+
+    public void insertIncomings(ArrayList<Incoming> incomingList) {
+
+    }
+
+}
