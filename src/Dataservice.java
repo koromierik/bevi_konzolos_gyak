80aa1daa7a119adc8c5698ab787117c555d522bc
MatiasX <waldorfosmatyi@gmail.com>
2024-04-17 11:17:32 +0200

    up

--- src/Dataservice.java
+++ src/Dataservice.java
@@ -28,13 +28,39 @@ public class DataService {
             ps.setString(2, customer.name);
             ps.setString(3, customer.email);
 
-            ps.executeUpdate();
+            ps.execute();
         }
         connection.close();
     }
 
     public void insertIncomings(ArrayList<Incoming> incomingList) {
 
-    }
+        String user = "bevi";
+        String password = "titok";
+        String url = "jdbc:mariadb://localhost:3306/bevi";
 
+        Connection connection = DriverManager.getConnection(url, user, password);
+
+        String sql = """
+                insert into customers(id,total,edate,source,customerId)
+
+                values(?,?,?,?,?)
+
+                """;
+
+        for (Incoming incoming : incomingList) {
+
+            PreparedStatement ps = connection.prepareStatement(sql); /// itt vannak egyesítve
+
+            ps.setInt(1, incoming.id);
+            ps.setDouble(2, incoming.total);
+            ps.setString(3, incoming.date);
+            ps.setString(4, incoming.source);
+            ps.setInt(5, incoming.customerId);
+
+            ps.execute();
+        }
+        connection.close();
+    }
+}
 }
